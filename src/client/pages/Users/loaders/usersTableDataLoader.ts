053fdabe82360a821be7152dad1a317ec19f6a31
Alexander M <tjnk24@gmail.com>
2022-08-22 18:56:43 +0400

    fix

--- src/client/pages/Users/loaders/usersTableDataLoader.ts
+++ src/client/pages/Users/loaders/usersTableDataLoader.ts
@@ -1,8 +1,7 @@
-import {store} from '__store/configureStore';
-import {createLoader} from '__utils/redux/createLoader/createLoader';
-import {UsersApiGetResponse} from '../api/types';
+import {createLoader} from '__utils/redux/createLoader';
 
+import {UsersApiGetResponse} from '../api/types';
 import {apiInstance} from '../api/usersApi';
 import {tableDataSlice} from '../slices/tableDataSlice';
 
-export const tableDataLoader = createLoader<UsersApiGetResponse[]>(tableDataSlice.actions, apiInstance.get)
+export const tableDataLoader = createLoader<UsersApiGetResponse[]>(tableDataSlice.actions, apiInstance.get);

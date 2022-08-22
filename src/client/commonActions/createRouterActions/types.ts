52792d3024e39d0fe956be8d3c8b0fbb727388cd
Alexander M <tjnk24@gmail.com>
2022-08-22 19:01:54 +0400

    fix

--- src/client/commonActions/createRouterActions/types.ts
+++ src/client/commonActions/createRouterActions/types.ts
@@ -4,6 +4,6 @@ import {RouterState} from '__utils/routing/types';
 
 export type RouterActionsKeyType = keyof typeof routerActions;
 
-export type RouterArgumentType = string | number | never;
+export type RouterArgumentType = string | number;
 
 export type RouterActionType = (arg0?: RouterArgumentType, arg1?: RouterState) => CallHistoryMethodAction;

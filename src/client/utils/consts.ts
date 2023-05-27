ac61be85bf5062ff88b319f8aee53263fde9ffe3
tjnk24 <tjnk24@gmail.com>
2023-05-27 12:01:58 +0400

    consts

---
+++ src/client/utils/consts.ts
@@ -0,0 +1,9 @@
+export const WINDOW_WIDTH_SMALL = 576;
+
+export const WINDOW_WIDTH_MEDIUM = 768;
+
+export const WINDOW_WIDTH_LARGE = 992;
+
+export const WINDOW_WIDTH_EXTRA_LARGE = 1200;
+
+export const WINDOW_WIDTH_DESKTOP_LARGE = 1366;
